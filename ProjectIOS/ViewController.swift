ffe8cf11e3389f141a43eb80dabafabb5c344cdf
Việt Nguyễn <nguyenviet@nguyens-mac-mini.local>
2018-07-31 14:09:11 +0700

    Initial Commit

---
+++ ProjectIOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectIOS
+//
+//  Created by Nguyễn Việt on 7/31/18.
+//  Copyright © 2018 Nguyễn Việt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
